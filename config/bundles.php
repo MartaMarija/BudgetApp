e428a00cb419ecdf067feb404f6725ce1752f9e8
MartaMarija <marta.picic@gmail.com>
2026-03-07 13:58:57 +0100

    Added twig

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
